1e6481da1da3ae0d6df8afafd3fe86d67f5ccc94
Arnaud Gelas <arnaudgelas@gmail.com>
2015-01-06 10:12:41 +0100

    fix update.py

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+Typical call
+============
+
+./update.py --json repositories.json -o /Users/arnaud/update/ -u arnaud -p 123456
--- update.py
+++ update.py
@@ -5,9 +5,15 @@ import sys
 import json
 import shlex
 import subprocess
+import argparse
+
+def sdz_url( user, password, address ):
+  address='http://' + user + ':' + password + '@' + address
+  return address
 
 def process_string( input_string ):
   cmd=input_string
+  print( cmd )
   args=shlex.split(cmd)
   val=subprocess.Popen(args)
   val.wait()
@@ -15,55 +21,82 @@ def process_string( input_string ):
 def update_project( remote, branches ):
   process_string( 'git fetch ' + remote )
   process_string( 'git fetch ' + remote + ' --tags' )
-
-#  process_string( 'git push sdz --tags' )
+  process_string( 'git push sdz --tags' )
 
   process_string( 'git fetch ' + remote )
 
   for b in branches:
     process_string( 'git checkout -t ' + remote + '/' + b )
+    process_string( 'git checkout ' + b )
     process_string( 'git pull ' + remote + ' ' + b )
+    process_string( 'git push sdz ' + b )
 
-#        print( 'git push sdz ' + b )
-#        process_string( 'git push sdz ' + b )
+if __name__ == "__main__":
+  parser=argparse.ArgumentParser()
+  parser.add_argument( "--json", help="input json file" )
+  parser.add_argument( '-o', "--output", help="output directory")
+  parser.add_argument( '-u', "--user", help="user name" )
+  parser.add_argument( '-p', "--password", help="password" )
+  args = parser.parse_args()
 
+  filename=args.json
+  output=args.output
+  user=args.user
+  password=args.password
 
-filename='repositories.json'
-output='/Users/arnaud/update/'
-json_file=open( filename )
+  wrongargs=False
 
-json_data=json.load( json_file )
+  if filename is None:
+    wrongargs=True
 
-projects=json_data["projects"]
+  if output is None:
+    wrongargs=True
 
-print( 'number of projects: ' + str( len( projects) ) )
+  if user is None:
+    wrongargs=True
 
-for p in projects:
-  name = p["name"]
-  print( '* update ' + name )
-  repositories=p["repositories"]
+  if password is None:
+    wrongargs=True
 
-  if os.path.isdir( os.path.join( output, name ) ):
-    if os.path.isdir( os.path.join( output, name, '.git' )):
-      for r in repositories:
-        update_project( r["remote"], r["branches"] )
+  if wrongargs:
+    parser.print_help()
+    sys.exit(1)
+
+  json_file=open( filename )
+  json_data=json.load( json_file )
+
+  projects=json_data["projects"]
+
+  os.chdir( output )
+  print( 'number of projects: ' + str( len( projects) ) )
+
+  for p in projects:
+    name = p["name"]
+    print( '* update ' + name )
+    repositories=p["repositories"]
+
+    if os.path.isdir( os.path.join( output, name ) ):
+      if os.path.isdir( os.path.join( output, name, '.git' )):
+        os.chdir( os.path.join( output, name ) )
+        for r in repositories:
+          update_project( r["remote"], r["branches"] )
+      else:
+        print( 'directory exists but not a git repository' )
+        sys.exit(1)
     else:
-      print( 'directory exists but not a git repository' )
-      sys.exit(1)
-  else:
-    print( 'git clone -o ' + repositories[0]["remote"] + ' ' + repositories[0]["url"] + ' ' + name )
+      os.chdir( output )
 
-    git_clone_cmd='git clone -o ' + repositories[0]["remote"] + ' ' + repositories[0]["url"] + ' ' + name
-    process_string( git_clone_cmd )
+      git_clone_cmd='git clone -o ' + repositories[0]["remote"] + ' ' + repositories[0]["url"] + ' ' + name
+      process_string( git_clone_cmd )
 
-    os.chdir( os.path.join( output, name ) )
+      os.chdir( os.path.join( output, name ) )
 
-#    git_remote_add_sdz_cmd='git remote add sdz ' + p["sdz"]
-#    process_string( git_remote_add_sdz_cmd )
+      git_remote_add_sdz_cmd='git remote add sdz ' + sdz_url( user, password, p["sdz"] )
+      process_string( git_remote_add_sdz_cmd )
 
-    for r in repositories:
-      if r is not repositories[0]:
-        process_string( 'git remote add ' + r["remote"] + ' ' + r["url"] )
-      update_project( r["remote"], r["branches"] )
+      for r in repositories:
+        if r is not repositories[0]:
+          process_string( 'git remote add ' + r["remote"] + ' ' + r["url"] )
+        update_project( r["remote"], r["branches"] )
 
-json_file.close()
+  json_file.close()
